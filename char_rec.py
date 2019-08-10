e86c2fe957514ca7a87cc3ab35b23208f0862b96
Fahad <fha0786@gmail.com>
2019-08-10 21:15:08 +0530

    GUI with tkinter working

--- char_rec.py
+++ char_rec.py
@@ -1,77 +1,74 @@
-from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Line, Color, Rectangle
+import tkinter as tk
+import numpy as np
 from predictor import Predictor
-from functools import partial
+from PIL import ImageGrab
 import os
-import random
 
 IMAGE_FOLDER = 'temp'
 
-class DrawInput(Widget):
-	def __init__(self, **kwargs):
-		super(DrawInput, self).__init__(**kwargs)
-		self.image_id = 1
-		self.predictor = Predictor()
-		with self.canvas.before:
-			Color(1, 1, 1, mode='rgb')
-			Rectangle(pos=self.pos, size=self.pos)
-		with self.canvas:
-			Color(0, 1, 0, mode='rgb')
 
-	def on_touch_down(self, touch):
-		with self.canvas:
-			touch.ud["line"] = Line(points=(touch.x, touch.y), width=5.0)
+class MainWindow(tk.Frame):
+    def __init__(self, master):
+        super(MainWindow, self).__init__(master)
+        self.pack()
+        self.master.title("Character Recognizer")
+        self.drawing_canvas = tk.Canvas(self, width=320, height=180)
+        self.drawing_canvas.pack()
+        self.drawing_canvas.bind('<Button-1>', self.click)
+        self.drawing_canvas.bind('<B1-Motion>', self.move)
+        self.bottom_frame = tk.Frame(self)
+        self.bottom_frame.pack(side='bottom', padx=15, pady=15)
+        self.button = tk.Button(self.bottom_frame, text="Process")
+        self.button.pack(side='left', padx=15, pady=15)
+        self.button.bind('<Button-1>', self.make_prediction)
+        self.clear_button = tk.Button(self.bottom_frame, text="Clear")
+        self.clear_button.pack(side='left', padx=15, pady=15)
+        self.clear_button.bind('<Button-1>', self.clear_canvas)
+        self.result = tk.Label(self.bottom_frame, text="Result")
+        self.result.pack(side='left', padx=15, pady=15)
+        self.prev = None
+        self.image_id = 1
+        self.get_predictor()
 
-	def on_touch_move(self, touch):
-		touch.ud["line"].points += (touch.x, touch.y)
+    def get_predictor(self):
+        self.predictor = Predictor()
 
-	def save_image(self, *args):
-		print("Button pressed")
-		self.image_id += 1
-		image_path = os.path.join(IMAGE_FOLDER, 'img' + str(self.image_id) + '.png')
-		self.export_to_png(image_path)
-		result = self.predictor.predict(image_path)
-		print("Final result: {}".format(result))
-		print(type(args[0]))
-		args[0].text = result
-		#text_box.text = result
+    def clear_canvas(self, event):
+        self.drawing_canvas.delete('all')
 
+    def make_prediction(self, event):
+        self.image_id += 1
+        image_path = os.path.join(IMAGE_FOLDER,
+                                  'img' + str(self.image_id) + '.png')
+        x = self.master.winfo_rootx() + self.drawing_canvas.winfo_x()
+        y = self.master.winfo_rooty() + self.drawing_canvas.winfo_y()
+        x1 = x + self.drawing_canvas.winfo_width()
+        y1 = y + self.drawing_canvas.winfo_height()
+        img = ImageGrab.grab().crop((x, y, x1, y1))
+        img.save(image_path)
+        image = np.array(img)
+        print("Numpy array shape is: {}".format(image.shape))
+        prediction = self.predictor.predict(image_path)
+        self.result.config(text=str(prediction))
 
+    def click(self, event):
+        self.prev = event
 
-class ScatterTextWidget(BoxLayout):
-	pass
+    def move(self, event):
+        self.drawing_canvas.create_line(self.prev.x, self.prev.y,
+                                        event.x, event.y, width=5)
+        self.prev = event
 
+    def perform_cleanup(self):
+        self.predictor.close_session()
 
-class TextReader(App):
-	# def __init__(self, **kwargs):
-	# 	super(TextReader, self).__init__(**kwargs)
-	# 	self.predictor = Predictor()
-		 
-	def build(self):
-		main_widget = ScatterTextWidget(orientation='vertical')
-		painter = DrawInput()
-		bottom_part = BoxLayout(orientation='horizontal')
-		b = Button(id='processor', font_size=100, size_hint_y=None,
-				   height=150, text='Process')#, on_press=painter.save_image)
-		text_box = TextInput(id='result', font_size=100,
-							 size_hint_y=None, height=150)
-
-		#b.bind(on_press=painter.save_image)
-		bottom_part.add_widget(b)
-		bottom_part.add_widget(text_box)
-		main_widget.add_widget(painter)
-		main_widget.add_widget(bottom_part)
-		button_callback = partial(painter.save_image, text_box)
-		b.bind(on_press=button_callback)
-		return main_widget
- 		
 
 if __name__ == '__main__':
-	obj = TextReader()
-	obj.run()
+    main = None
+    try:
+        root = tk.Tk()
+        main = MainWindow(root)
+        root.mainloop()
+    finally:
+        if main:
+            main.perform_cleanup()
--- predictor.py
+++ predictor.py
@@ -2,58 +2,61 @@ import numpy as np
 import cv2
 import os
 import tensorflow as tf
-from tensorflow.contrib.layers import flatten
 from architecture import Lenet
 
+
 def normalize_images(x):
-    x = (x-128)/256.0
+    x = (x - 128) / 256.0
     return x
 
 
 class Predictor:
-	def __init__(self):
-		self.x = tf.placeholder(tf.float32, (None, 32, 32, 1))
-		self.y = tf.placeholder(tf.int32, (None))
-		self.one_hot_y = tf.one_hot(self.y, 10)
-		self.logits = Lenet().model(self.x)
-		self.sess = self.load_model()
+    def __init__(self):
+        self.x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+        self.y = tf.placeholder(tf.int32, (None))
+        self.one_hot_y = tf.one_hot(self.y, 10)
+        self.logits = Lenet().model(self.x)
+        self.sess = self.load_model()
 
-	def load_model(self):
-		saver = tf.train.Saver()
-		sessn = tf.Session()
-		saver.restore(sessn, tf.train.latest_checkpoint('../models'))
-		print("Loaded the graph...")
-		return sessn
+    def load_model(self):
+        saver = tf.train.Saver()
+        sessn = tf.Session()
+        saver.restore(sessn, tf.train.latest_checkpoint('../models'))
+        print("Loaded the graph...")
+        return sessn
 
-	def get_maxprob_idx(self, probs):
-		max_prob_idx = None
-		max_prob = -1
-		for i in range(len(probs)):
-			if probs[i] > max_prob:
-				max_prob = probs[i]
-				max_prob_idx = i
-		return max_prob_idx
+    def get_maxprob_idx(self, probs):
+        max_prob_idx = None
+        max_prob = -1
+        for i in range(len(probs)):
+            if probs[i] > max_prob:
+                max_prob = probs[i]
+                max_prob_idx = i
+        return max_prob_idx
 
-	def predict(self, image_file):
-		image = cv2.imread(image_file, 0)
-		image = cv2.resize(image, (32, 32))
-		image = normalize_images(image)
-		image_vector = np.reshape(image, (1, 32, 32, 1))
-		pred = tf.nn.softmax(self.logits)
-		print("Shape of Image: {}".format(image.shape))
-		classification = self.sess.run(pred, feed_dict={self.x: image_vector})
-		print("Result of classification: {}".format(classification))
-		print("Performed the classification")
-		probs = classification[0]
-		idx = np.argmax(probs)
-		self.sess.close()
-		print("Predicted number = {}".format(idx))
-		return idx
+    def predict(self, image_file):
+        image = cv2.imread(image_file, 0)
+        image = cv2.resize(image, (32, 32))
+        image = normalize_images(image)
+        image_vector = np.reshape(image, (1, 32, 32, 1))
+        pred = tf.nn.softmax(self.logits)
+        print("Shape of Image: {}".format(image.shape))
+        classification = self.sess.run(pred, feed_dict={self.x: image_vector})
+        print("Result of classification: {}".format(classification))
+        print("Performed the classification")
+        probs = classification[0]
+        idx = np.argmax(probs)
+        print("Predicted number = {}".format(idx))
+        return idx
+
+    def close_session(self):
+        self.sess.close()
+        print("Tensorflow session closed")
 
 if __name__ == '__main__':
-	predictor = Predictor()
-	image_file = 'temp/img1.png'
-	predictor.predict(image_file)
+    predictor = Predictor()
+    image_file = 'temp/img1.png'
+    predictor.predict(image_file)
 
 
 
